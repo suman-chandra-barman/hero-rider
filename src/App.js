134f077b3fd8cbdcd958a172b729742ba02e7754
Suman-Chandra-Barman <sumandranathbarman@gmail.com>
2023-03-21 08:46:42 +0600

    layout and router create and provide

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Route";
+
 function App() {
-  return <div>hi</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ src/router/route.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+export default router;
